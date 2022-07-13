3c38dc279c4276e5baca223c370948a78a93e3d8
AndreeaSimonaC <ciobanu_a_simona@yahoo.com>
2022-07-13 20:31:02 +0300

    PracticeOne

--- src/NumberOfGrades.java
+++ src/NumberOfGrades.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProblemOne {
+public class NumberOfGrades {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
--- src/NumbersDivisibleBySix.java
+++ src/NumbersDivisibleBySix.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class ProblemTwo {
+public class NumbersDivisibleBySix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfElements = scanner.nextInt();
