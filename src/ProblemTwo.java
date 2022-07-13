040117f74c93f2272eeb5c3783d9654b4dadd8bc
AndreeaSimonaC <ciobanu_a_simona@yahoo.com>
2022-07-13 20:23:24 +0300

    PracticeOne

---
+++ src/ProblemTwo.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class ProblemTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        int nextNumber;
+        int sum = 0;
+        for (int i = 1; i <= numberOfElements; i++) {
+            nextNumber = scanner.nextInt();
+            if (nextNumber % 6 == 0) {
+                sum += nextNumber;
+            }
+        } System.out.println(sum);
+    }
+}
